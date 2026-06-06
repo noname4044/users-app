7df2205094e02e4009a104b5eaffdfdcd9e7c899
Noname <user@MacBook-Air-Noname.local>
2026-06-07 01:55:39 +0300

    fix render port detection

--- server.js
+++ server.js
@@ -54,6 +54,10 @@ const server = http.createServer((req, res) => {
     }
 })
 
-server.listen(5137, '0.0.0.0', () => {
-    console.log('Сервер запущен!');
+const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 10000;
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log("Server running on port:", PORT);
 });
